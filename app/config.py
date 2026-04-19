595d956713eda3467150b23896e3d3a11687910f
Jonathan Chau <chaujt1@uci.edu>
2026-04-18 19:03:56 -0700

    Error checking in config.py

--- app/config.py
+++ app/config.py
@@ -8,6 +8,9 @@ SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_ANON_KEY = os.getenv("SUPABASE_ANON_KEY")
 SUPABASE_SERVICE_ROLE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
 
+if not SUPABASE_URL or not SUPABASE_ANON_KEY or not SUPABASE_SERVICE_ROLE_KEY:
+    raise ValueError("Missing required Supabase environment variables")
+
 # public client - RLS
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_ANON_KEY)
 
